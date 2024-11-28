23e9816c67fb484a80e46f19b26745a48091ca3b
jmNgoroAsyst <jmngabirano@asystresources.com>
2024-11-28 16:32:05 +0200

    project setup

---
+++ src/main/java/com/Karyuryu/dev/Banking/BankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.Karyuryu.dev.Banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppApplication.class, args);
+	}
+
+}
